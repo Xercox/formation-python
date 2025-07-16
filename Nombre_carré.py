9db4f15a31ba7dcc0ba1fea24c5cb7f47d8496ce
Xercox <jonathan1994dejaifve@gmail.com>
2025-07-16 16:02:51 +0200

    Ajout d'exemples de boucle for : génération avec range et parcours d'une liste

---
+++ Nombre_carré.py
@@ -0,0 +1,10 @@
+# # Génération Sans liste.
+# for nombre in range (10):
+#     print(nombre ** 2)
+
+# Avec liste
+
+liste = [0,1,2,3,4,5,6,7,8,9]
+
+for nombre in liste[0:10]:
+    print(nombre ** 2)
